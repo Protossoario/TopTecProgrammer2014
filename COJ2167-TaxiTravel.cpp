efc759c99dc47b4ebe139345449efb1775c9b8c6
Protossoario <lalo.sa.94@gmail.com>
2014-02-05 10:39:15 -0600

    Rechazado

--- COJ2167-TaxiTravel.cpp
+++ COJ2167-TaxiTravel.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
+#include <set>
 #include <cmath>
 using namespace std;
 
 int main() {
 	int T, N, S, s;
+	int taxis[4] = {0, 0, 0, 0};
 	cin >> T;
 	while (T--) {
 		cin >> N;
 		S = 0;
 		while (N--) {
 			cin >> s;
-			S += s;
+			if (s != 4) {
+				taxis[s]++;
+			}
+			else {
+				S++;
+			}
 		}
-		cout << ((S / 4) + (S % 4 == 0 ? 0 : 1)) << endl;
+		int min = (taxis[1] < taxis[3] ? taxis[1] : taxis[3]);
+		S += min;
+		taxis[3] -= min;
+		taxis[1] -= min;
+		S += taxis[2] / 2;
+		taxis[2] = taxis[2] % 2;
+		min = (taxis[1] < taxis[2] ? taxis[1] : taxis[2]);
+		taxis[3] += min;
+		taxis[1] -= min;
+		taxis[2] -= min;
+		min = (taxis[1] < taxis[3] ? taxis[1] : taxis[3]);
+		S += min;
+		taxis[3] -= min;
+		taxis[1] -= min;
+		for (int i = 1; i < 4; i++) {
+			if (taxis[i] > 0) {
+				S += taxis[i];
+				taxis[i] = 0;
+			}
+		}
+		cout << S << endl;
 	}
 }
