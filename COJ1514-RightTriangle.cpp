cdd576cc56a7228c5456996ddc63d0a3f610681a
Lalo <lalo.sa.94@gmail.com>
2014-02-08 22:36:15 -0600

    Aceptado

--- COJ1514-RightTriangle.cpp
+++ COJ1514-RightTriangle.cpp
@@ -1,16 +1,43 @@
-#include <iostream>
-#include <iomanip>
+#include <cstdio>
+#include <cstring>
 #include <cmath>
 using namespace std;
 
+double area(const double A, const double B) {
+	return pow((A * B) / (A + B),2);
+}
+
+void trimZeroes(char * s, int n) {
+	s = strchr(s, '.');
+	if (s != NULL) {
+		while (n >= 0) {
+			n--;
+			if (*s != '\0') {
+				s++;
+			}
+			else {
+				break;
+			}
+		}
+		*s-- = '\0';
+		while (*s == '0') {
+			*s-- = '\0';
+		}
+		if (*s == '.') {
+			*s = '\0';
+		}
+	}
+}
+
 int main() {
 	int N;
-	double B, C;
-	cout << fixed << setprecision(4);
-	cin >> N;
+	double A, B;
+	scanf("%i", &N);
 	while (N--) {
-		cin >> B >> C;
-		double diagonal = (B * C) / sqrt(pow(B, 2) + pow(C,2));
-		cout << (pow(diagonal, 2) / 2) << endl;
+		char result[12];
+		scanf("%lf %lf", &A, &B);
+		sprintf(result, "%.4lf", area(A, B));
+		trimZeroes(result, 4);
+		printf("%s\n", result);
 	}
 }
