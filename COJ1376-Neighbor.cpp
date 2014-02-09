acd16b6653477c80210e4c7d25d3eaaee151b2b5
Lalo <lalo.sa.94@gmail.com>
2014-02-08 22:52:26 -0600

    Compilation Error???

---
+++ COJ1376-Neighbor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string num;
+	cin >> num;
+	if (next_permutation(num.begin(), num.end())) {
+		cout << num << endl;
+	}
+	else {
+		cout << "0" << endl;
+	}
+	return 0;
+}
