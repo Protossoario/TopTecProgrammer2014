250e901188ab36dce5db6c3096d4d6008fea162b
Lalo <lalo.sa.94@gmail.com>
2014-02-22 17:03:59 -0600

    Aceptado

--- COJ2604-ArithmeticWithMorse.cpp
+++ COJ2604-ArithmeticWithMorse.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 using namespace std;
 
-int parseMorse(string s) {
+int parseMorse(const string &s) {
 	int res = 0;
 	for (unsigned int i = 0; i < s.length(); i++) {
 		switch (s[i]) {
@@ -24,12 +24,25 @@ int parseMorse(string s) {
 	return res;
 }
 
-int evaluate(int A, int B, char op) {
+int evaluate(const int &A, const int &B, const char &op) {
 	switch (op) {
 		case '+':
 			return A + B;
 		case '*':
 			return A * B;
+	}
+	return 0;
+}
+
+int compareOps(const char &A, const char &B) {
+	if (A == '+') {
+		if (B == '+') {
+			return 0;
+		}
+		return -1;
+	}
+	if (B == '+') {
+		return 1;
 	}
 	return 0;
 }
@@ -46,9 +59,28 @@ int main(int argc, const char *argv[])
 	numbers.push(parseMorse(s));
 	for (int i = 0; i < N; i++) {
 		cin >> s;
+		while (!operators.empty() && (compareOps(operators.top(), s[0]) >= 0)) {
+			B = numbers.top();
+			numbers.pop();
+			A = numbers.top();
+			numbers.pop();
+			op = operators.top();
+			operators.pop();
+			numbers.push(evaluate(A, B, op));
+		}
 		operators.push(s[0]);
 		cin >> s;
 		numbers.push(parseMorse(s));
 	}
+	while (!operators.empty()) {
+		B = numbers.top();
+		numbers.pop();
+		A = numbers.top();
+		numbers.pop();
+		op = operators.top();
+		operators.pop();
+		numbers.push(evaluate(A, B, op));
+	}
+	cout << numbers.top() << endl;
 	return 0;
 }
