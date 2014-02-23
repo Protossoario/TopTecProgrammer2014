280139282a1790198453388e2f064aa44b52110d
Lalo <lalo.sa.94@gmail.com>
2014-02-22 18:03:59 -0600

    Wrong Answer Test 1

---
+++ COJ2019-Primality.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+bool isPrime(int N) {
+	if (N % 2 == 0 && N > 2) {
+		return false;
+	}
+	int i = 3;
+	while ((i * i) <= N) {
+		if (N % i == 0) {
+			return false;
+		}
+		i += 2;
+	}
+	return true;
+}
+int main(int argc, const char *argv[])
+{
+	string N0, N;
+	int perm, primality = 0;
+	cin >> N;
+	N0 = N;
+	do {
+		perm = atoi(N.c_str());
+		if (isPrime(perm)) {
+			primality++;
+		}
+		next_permutation(N.begin(), N.end());
+	} while (N != N0);
+	cout << primality << endl;
+	return 0;
+}
