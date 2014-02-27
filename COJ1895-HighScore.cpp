726081b87855fd40d8ff1b07454d8cc9bc491057
Protossoario <lalo.sa.94@gmail.com>
2014-02-26 18:10:54 -0600

    Pendiente de probar

--- COJ1895-HighScore.cpp
+++ COJ1895-HighScore.cpp
@@ -20,15 +20,6 @@
 #include <string>
 #include <climits>
 using namespace std;
-class Node {
-public:
-	Node * left, * right;
-	int weightLeft, weightRight;
-	Node() {
-		left = right = NULL;
-		weightLeft = weightRight = INT_MAX;
-	}
-};
 int min(const int &a, const int &b) {
 	return a < b ? a : b;
 }
@@ -38,57 +29,56 @@ int minOffset(const char &c) {
 int main(int argc, const char *argv[])
 {
 	string s;
-	int N, sum, last_dist;
-	Node * head, * tail;
+	int N, sum, ind1, min, min_ind1, min_ind2;
+	bool A;
 	cin >> N;
 	while (N--) {
 		cin >> s;
-		sum = last_dist = 0;
-		head = tail = new Node();
-		if (s[0] != 'A') {
-			sum += minOffset(s[0]);
-		}
-		for (unsigned int i = 1; i < s.length(); i++) {
-			last_dist++;
+		min = INT_MAX;
+		sum = 0;
+		ind1 =  min_ind1 = min_ind2 = -1;
+		A = false;
+		for (unsigned int i = 0; i < s.length(); i++) {
 			if (s[i] != 'A') {
 				sum += minOffset(s[i]);
-				Node * temp = tail;
-				tail = new Node();
-				temp->right = tail;
-				tail->left = temp;
-				temp->weightRight = tail->weightLeft = last_dist;
-				last_dist = 0;
+				if (A) {
+					A = false;
+					int dist = i - ind1;
+					if (dist < min) {
+						min = dist;
+						min_ind1 = ind1;
+						min_ind2 = i;
+					}
+				}
+			}
+			else if (!A) {
+				ind1 = i;
+				A = true;
 			}
 		}
-		if (head != tail) {
-			head->left = tail;
-			tail->right = head;
-			head->weightLeft = tail->weightRight = last_dist + 1;
-			tail = head;
-			if (head->weightLeft < head->weightRight) {
-				sum += head->weightLeft;
-				head = head->left;
+		if (A) {
+			ind2 = s.length();
+		}
+		if (min_ind1 == -1) {
+			sum += s.length() - 1;
+		}
+		else if (min_ind1 == 0) {
+			sum += s.length() - min;
+		}
+		else if (min_ind2 == s.length()) {
+			sum += s.length() - min - 1;
+		}
+		else {
+			int left = s.length() - min - 1;
+			int right = s.length() - min;
+			if (left < right) {
+				sum += left;
 			}
 			else {
-				sum += head->weightRight;
-				tail = head->right;
-			}
-			while (head != tail->right) {
-				if (head->weightLeft < tail->weightRight) {
-					sum += head->weightLeft;
-				}
-				else {
-					sum += tail->weightRight;
-					tail = tail->right;
-				}
-			}
-			while (head->right != head) {
-				Node * temp = head->right;
-				head->right = head->right->right;
-				delete temp;
+				sum += right;
 			}
 		}
-		delete head;
+		cout << min << " " << min_ind1 << " " << min_ind2 << endl;
 		cout << sum << endl;
 	}
 	return 0;
